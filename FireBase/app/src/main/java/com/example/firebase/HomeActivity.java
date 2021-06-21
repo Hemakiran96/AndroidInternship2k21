3890a1bfea81e001a29fa01dbb0ae65ccde97385
hemakiran <82698622+Hemakiran96@users.noreply.github.com>
2021-06-21 18:37:29 +0530

    base

---
+++ FireBase/app/src/main/java/com/example/firebase/HomeActivity.java
@@ -0,0 +1,24 @@
+package com.example.firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompatExtras;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+    TextView tv;
+    FirebaseAuth auth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        tv = findViewById(R.id.textView);
+        auth = FirebaseAuth.getInstance();
+        tv.setText("welcome"+auth.getCurrentUser().getEmail());
+    }
+}
