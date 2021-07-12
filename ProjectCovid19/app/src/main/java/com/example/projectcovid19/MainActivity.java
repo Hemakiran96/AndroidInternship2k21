72af9f67caa588144e564fbf831c8ed979d5133c
hemakiran <82698622+Hemakiran96@users.noreply.github.com>
2021-07-13 01:30:55 +0530

    project

--- ProjectCovid19/app/src/main/java/com/example/projectcovid19/MainActivity.java
+++ ProjectCovid19/app/src/main/java/com/example/projectcovid19/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.projectcovid19;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void search(View view) {
+
+    }
+
+    public void india(View view) {
+    }
+
+    public void england(View view) {
+    }
+
+    public void japan(View view) {
+    }
+
+    public void nepal(View view) {
+    }
+
+    public void usa(View view) {
+    }
+
+    public void uk(View view) {
+    }
+
+    public void china(View view) {
+    }
+
+    public void pk(View view) {
     }
 }
